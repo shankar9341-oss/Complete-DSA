0dabcffb38ef36d79a68183c54138cb221881587
shankar9341-oss <shankardas9341@gmail.com>
2026-08-08 20:33:49 +0530

    Time: 0 ms (100%), Space: 26.5 MB (44.38%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -4,21 +4,24 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         k = k % len(nums)
+        nums.reverse()
+        nums[:k] = reversed(nums[:k])
+        nums[k:] = reversed(nums[k:])
         
-        left, right = 0, len(nums)-1
-        while left < right:
-            nums[left], nums[right] = nums[right], nums[left]
-            left += 1
-            right -= 1
+        # left, right = 0, len(nums)-1
+        # while left < right:
+        #     nums[left], nums[right] = nums[right], nums[left]
+        #     left += 1
+        #     right -= 1
 
-        left, right = 0,k-1
-        while left < right:
-            nums[left], nums[right] = nums[right], nums[left]
-            left += 1
-            right -= 1
+        # left, right = 0,k-1
+        # while left < right:
+        #     nums[left], nums[right] = nums[right], nums[left]
+        #     left += 1
+        #     right -= 1
 
-        left, right = k, len(nums)-1
-        while left < right:
-            nums[left], nums[right] = nums[right], nums[left]
-            left += 1
-            right -= 1
+        # left, right = k, len(nums)-1
+        # while left < right:
+        #     nums[left], nums[right] = nums[right], nums[left]
+        #     left += 1
+        #     right -= 1
