47ef4ee6dbdfec0b0ae7692f527038388baed93c
Shankar Das <shankardas9341@gmail.com>
2026-08-17 18:39:49 +0530

    Time: 13 ms (23.17%), Space: 19.2 MB (54.6%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        temp = x
+        result = 0
+        while temp > 0:
+            rev = temp % 10
+            temp //= 10
+            result = result * 10 + rev
+        return result == x
+
+          
+        
