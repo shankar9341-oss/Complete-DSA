393edd8c2d1fd46a5f768797f1f93b463886c3d8
Shankar Das <shankardas9341@gmail.com>
2026-08-19 07:33:52 +0530

    Time: 119 ms (5.91%), Space: 23.2 MB (32.39%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.py
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.py
@@ -1,16 +1,17 @@
 class Solution:
     def maxNumberOfFamilies(self, n: int, reservedSeats: List[List[int]]) -> int:
         res = 0
-        mapp = defaultdict(list)
-        for r,char in reservedSeats:
-            mapp[r - 1].append(char - 1)
+        mapp = defaultdict(set)
+        for r,c in reservedSeats:
+            mapp[r - 1].add(c - 1)
 
-        row = 0
-        for k in mapp:
-            row += 1
-            v1 = all(not i in mapp[k] for i in range(1, 5))
-            v2 = all(not i in mapp[k] for i in range(3, 7))
-            v3 = all(not i in mapp[k] for i in range(5, 9))
-            res += max(v2, v1 + v3)
-
-        return res + (n - row) * 2
+        for row in mapp.values():
+        
+            left = all(i not in row for i in range(1, 5))
+            mid = all(i not in row for i in range(3, 7))
+            right = all(i not in row for i in range(5, 9))
+            if left and right:
+                res += 2
+            elif left or mid or right:
+                res += 1
+        return res + (n - len(mapp)) * 2
